86eff3191cd722bbda47549995d04b5491e14c46
newaz08 <newazsharifamit@yahoo.com>
2022-06-13 22:30:49 +0600

    second commit

--- src/main/java/com/example/docker/DockerApplication.java
+++ src/main/java/com/example/docker/DockerApplication.java
@@ -1,7 +1,5 @@
 package com.example.docker;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
@@ -11,18 +9,13 @@ import javax.annotation.PostConstruct;
 @SpringBootApplication
 public class DockerApplication {
 
-	public static Logger logger = LoggerFactory.getLogger(DockerApplication.class);
 
-	@PostConstruct
-	public void init() {
-		logger.info("Application Started...");
-	}
 	//@GetMapping("/hello")
 	public String greetings(){
 		return "Hello";
 	}
 	public static void main(String[] args) {
-		logger.info("Application executed....");
+
 		SpringApplication.run(DockerApplication.class, args);
 	}
 
--- src/test/java/com/example/docker/DockerApplicationTests.java
+++ src/test/java/com/example/docker/DockerApplicationTests.java
@@ -1,18 +1,14 @@
 package com.example.docker;
 
-import com.sun.org.slf4j.internal.Logger;
-import com.sun.org.slf4j.internal.LoggerFactory;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class DockerApplicationTests {
 
-	public static Logger logger = LoggerFactory.getLogger(DockerApplicationTests.class);
-	@Test
+
 	void contextLoads() {
-		logger.info("Test Cases executing...");
-		assertEquals(true, true);
+
 	}
 
 }
